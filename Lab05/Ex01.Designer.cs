9586264c2a0234fcaa87d836c5759d78acfbbcd6
HTHai-Nguyen <21520808@ms.uit.edu.vn>
2024-12-18 11:53:36 +0700

    done exercise01

---
+++ Lab05/Ex01.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Lab05
+{
+    partial class Ex01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.richTextBody = new System.Windows.Forms.RichTextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBody.Location = new System.Drawing.Point(29, 214);
+            this.lblBody.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(63, 25);
+            this.lblBody.TabIndex = 18;
+            this.lblBody.Text = "Body:";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(162, 146);
+            this.txtSubject.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(562, 30);
+            this.txtSubject.TabIndex = 17;
+            // 
+            // lblSub
+            // 
+            this.lblSub.AutoSize = true;
+            this.lblSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSub.Location = new System.Drawing.Point(29, 149);
+            this.lblSub.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(84, 25);
+            this.lblSub.TabIndex = 16;
+            this.lblSub.Text = "Subject:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.Location = new System.Drawing.Point(162, 83);
+            this.txtTo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(337, 30);
+            this.txtTo.TabIndex = 15;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrom.Location = new System.Drawing.Point(162, 37);
+            this.txtFrom.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(337, 30);
+            this.txtFrom.TabIndex = 14;
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.Location = new System.Drawing.Point(29, 88);
+            this.lblTo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(36, 25);
+            this.lblTo.TabIndex = 13;
+            this.lblTo.Text = "To";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.Location = new System.Drawing.Point(29, 37);
+            this.lblFrom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(57, 25);
+            this.lblFrom.TabIndex = 12;
+            this.lblFrom.Text = "From";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(613, 49);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(111, 49);
+            this.btnSend.TabIndex = 11;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // richTextBody
+            // 
+            this.richTextBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBody.Location = new System.Drawing.Point(162, 214);
+            this.richTextBody.Name = "richTextBody";
+            this.richTextBody.Size = new System.Drawing.Size(562, 224);
+            this.richTextBody.TabIndex = 19;
+            this.richTextBody.Text = "";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDel.Location = new System.Drawing.Point(162, 470);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(123, 41);
+            this.btnDel.TabIndex = 20;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(601, 470);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(123, 41);
+            this.btnClose.TabIndex = 20;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 534);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.richTextBody);
+            this.Controls.Add(this.lblBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Exercise01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox richTextBody;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Lab05/Ex01.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Lab05
+{
+    public partial class Ex01 : Form
+    {
+        public Ex01()
+        {
+            InitializeComponent();
+        }
+
+        private string smtp_pass = "1234qwer";
+        private CheckBox chkIsHtml;
+        
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Validate input fields
+            if (string.IsNullOrWhiteSpace(txtFrom.Text) ||
+                string.IsNullOrWhiteSpace(txtTo.Text) ||
+                //string.IsNullOrWhiteSpace(txtPassword.Text) ||
+                string.IsNullOrWhiteSpace(txtSubject.Text))
+            {
+                MessageBox.Show("Please fill in all required fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Create SMTP client for local network (internal mail)
+                using (SmtpClient smtpClient = new SmtpClient("smtp.gmail.com"))
+                {
+                    // Get email details from form
+                    string mailFrom = txtFrom.Text.Trim();
+                    string mailTo = txtTo.Text.Trim();
+                    //string password = txtPassword.Text.Trim();
+
+                    // Set up network credentials
+                    //var basicCredential = new NetworkCredential(mailFrom, password);
+
+                    // Configure SMTP 
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new NetworkCredential("21520808@gm.edu.vn", smtp_pass) ;
+
+                    // Create mail message
+                    using (MailMessage message = new MailMessage())
+                    {
+                        MailAddress fromAddress = new MailAddress(mailFrom);
+
+                        message.From = fromAddress;
+                        message.Subject = txtSubject.Text.Trim();
+
+                        // Support for HTML emails
+                        message.IsBodyHtml = chkIsHtml.Checked;
+                        message.Body = richTextBody.Text;
+
+                        message.To.Add(mailTo);
+
+                        
+
+                        // Send email
+                        smtpClient.Send(message);
+
+                        MessageBox.Show("Email sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Clear form after successful send
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Detailed error handling
+                MessageBox.Show($"Error sending email: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            txtFrom.Clear();
+            txtTo.Clear();
+            txtSubject.Clear();
+            richTextBody.Clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            new Dashboard().Show();
+            this.Hide();
+        }
+    }
+}
